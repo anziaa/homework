f09370540ea415212d71794634365ab779860fa3
anziaa <2375259233@qq.com>
2022-09-11 13:44:29 +0800

    test

---
+++ src/hw01.java
@@ -0,0 +1,8 @@
+public class hw01 {
+    public static void main(String[] args){
+        System.out.println("姓名:张智豪");
+        System.out.println("性别：男");
+        System.out.println("籍贯：汉");
+        System.out.println("住址：武汉大学");
+    }
+}
